f9c5daebc00f8509528ac7f4dbb8eee85038fcc1
quan <gquan97@gmail.com>
2025-06-29 19:35:01 +0700

    update GUI and .gitignore

--- UnetiExamSolution/UnetiExam.GUI/Properties/AssemblyInfo.cs
+++ UnetiExamSolution/UnetiExam.GUI/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("36dfb265-c530-4c06-84d8-1672dbd4d527")]
+[assembly: Guid("367b47c5-72d7-466f-b9a0-d3fb47da05d4")]
 
 // Version information for an assembly consists of the following four values:
 //
---
+++ UnetiExamSolution/UnetiExam.GUI/bin/Debug/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UnetiExam.GUI
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void guna2HtmlLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void htmlLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void guna2ImageButton1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void asd_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            LoginForm loginForm = new LoginForm();
+            loginForm.Show();
+
+            // Close this form when login form is closed
+            loginForm.FormClosed += (s, args) => this.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
